c7472a8c0ea4e64828aeb3ec2b56e9f54d2f78f1
Marjan Sadat <marjansadat10@gmail.com>
2020-11-07 12:17:47 -0800

    Portfolio Project

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import './App.css';
+import Row from './Row';
+import requests from './requests';
+import Banner from './Banner';
+import Nav from './Nav';
+
+function App() {
+  return (
+    <div className="app">
+      <Nav />
+      <Banner />
+      <Row title="Marjan Khanom Jan MA Love You Jaana Delam" isLargeRow/>
+      <Row 
+        title="NETFLIX ORIGINALS" 
+        fetchUrl={requests.fetchNetflixOriginals} 
+        isLargeRow 
+      />
+      <Row 
+        title="Trending Now" 
+        fetchUrl={requests.fetchTrending} 
+      />
+      <Row 
+        title="Top Rated" 
+        fetchUrl={requests.fetchTopRated} 
+      />
+      <Row 
+        title="Action Movies" 
+        fetchUrl={requests.fetchActionMovies} 
+      />
+      <Row 
+        title="Comedy Movies" 
+        fetchUrl={requests.fetchComedyMovies} 
+      />
+      <Row 
+        title="Horror Movies" 
+        fetchUrl={requests.fetchHorrorMovies} 
+      />
+      <Row 
+        title="Rpmance Movies" 
+        fetchUrl={requests.fetchRomanceMovies} 
+      />
+      <Row 
+        title="Documentarues" 
+        fetchUrl={requests.fetchDocumentries} 
+      />
+
+    </div>
+  );
+}
+
+export default App;
